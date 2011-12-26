c9cf5653e97af46df04a59fd99a43c0dd0032b2e
Chris Lloyd <christopher.lloyd@gmail.com>
2011-12-26 21:57:44 +1100

    Cleans ups the code and lets it import events.

--- lib/em/mixpanel.rb
+++ lib/em/mixpanel.rb
@@ -1,2 +1,54 @@
+require 'base64'
+require 'json'
 require 'em/mixpanel/version'
-require 'em/mixpanel/tracker'
+
+module EM
+  class Mixpanel
+    TRACK_URI = 'http://api.mixpanel.com/track'
+    IMPORT_URI = 'http://api.mixpanel.com/import'
+    
+    attr_reader :token
+    
+    def initialize(token, ip=nil)
+      @token, @ip = token, ip
+    end
+    
+    def track(event, properties={})
+      data = self.class.encode_data(event, {
+        token: token,
+        time: Time.now.to_i,
+        ip: ip
+      }.merge(properties))
+      
+      EM::HttpRequest.new(TRACK_URI.to_s).post(
+        body: {data: data},
+        query: {ip: 0}
+      )
+    end
+    
+    def import(event, properties={})
+      data = self.class.encode_data(event, {
+        token: token
+      }.merge(properties))
+      
+      EM::HttpRequest.new(IMPORT_URI.to_s).get(
+        query: {
+          data: data,
+          api_key: token
+        }
+      )
+    end
+    
+  private
+    
+    def self.encode_data(event, properties)
+      params = {event: event, properties: properties}
+      Base64.strict_encode64 params.to_json
+    end
+    
+    def ip
+      @ip || '1'
+    end
+    
+  end
+end
